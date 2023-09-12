a797215918475c7be72bf2ce19ce8f6a7c116385
Lee Forster <lee.forster28@gmail.com>
2023-09-12 15:15:09 +1000

    Working prototype of Go Server for the processing of passing all assets

--- routes/api.go
+++ routes/api.go
@@ -4,6 +4,8 @@ package routes
 
 import (
 	"database/sql"
+	"fmt"
+	"strings"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -16,11 +18,9 @@ type AssetDetail struct {
 	categoryId int
 }
 
-// Create the fiber handler
 func PassAllAssets(db *sql.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		// Get the locationId and userId from the request body
-
 		var Request struct {
 			LocationId int    `json:"locationId"`
 			UserId     string `json:"userId"`
@@ -33,9 +33,9 @@ func PassAllAssets(db *sql.DB) fiber.Handler {
 			})
 		}
 
-		// TODO: Call all the helper functions to create all of the asset checks
-
-		assets, err := fetchAssetsForLocation(Request.LocationId, db)
+		initialLocation := []int{Request.LocationId}
+		// Fetch all the locations under and including the provided location
+		allLocations, err := fetchChildLocations(initialLocation, db)
 		if err != nil {
 			return c.Status(500).JSON(fiber.Map{
 				"success": false,
@@ -43,8 +43,29 @@ func PassAllAssets(db *sql.DB) fiber.Handler {
 			})
 		}
 
+		// Convert the locations slice to a map for easier batch processing
+		locationMap := make(map[int]struct{})
+		for _, loc := range allLocations {
+			locationMap[loc] = struct{}{}
+		}
+
+		// Fetch assets for all these locations
+		assetsMap, err := fetchAssetsForLocations(allLocations, db)
+		if err != nil {
+			return c.Status(500).JSON(fiber.Map{
+				"success": false,
+				"message": err.Error(),
+			})
+		}
+
+		// Collate all assets from the map into a single slice
+		var allAssets []AssetDetail
+		for _, assets := range assetsMap {
+			allAssets = append(allAssets, assets...)
+		}
+
 		categoryIds := make(map[int]struct{})
-		for _, asset := range assets {
+		for _, asset := range allAssets {
 			categoryIds[asset.categoryId] = struct{}{}
 		}
 
@@ -64,8 +85,7 @@ func PassAllAssets(db *sql.DB) fiber.Handler {
 			})
 		}
 
-		err = processAndInsertAssets(assets, checkGroupTemplatesMap, checkItemTemplatesMap, Request.UserId, db)
-
+		err = processAndInsertAssets(allAssets, checkGroupTemplatesMap, checkItemTemplatesMap, Request.UserId, db)
 		if err != nil {
 			return c.Status(500).JSON(fiber.Map{
 				"success": false,
@@ -80,31 +100,40 @@ func PassAllAssets(db *sql.DB) fiber.Handler {
 	}
 }
 
-// Implement first handeler function to get all of the assets by the locationId
-// This function is incomplete as it needs to traverse all childLocations as well
-// TODO!!!
-func fetchAssetsForLocation(locationId int, db *sql.DB) ([]AssetDetail, error) {
-	const query = `
+func fetchAssetsForLocations(locationIds []int, db *sql.DB) (map[int][]AssetDetail, error) {
+	// Convert the slice of location IDs into a comma-separated string.
+	placeholders := strings.TrimSuffix(strings.Repeat("?,", len(locationIds)), ",")
+
+	const baseQuery = `
         SELECT a.id AS assetId, p.id AS productId, p.categoryId
         FROM Assets AS a
         JOIN Product AS p ON a.productId = p.id
-        WHERE a.locationId = ?
+        WHERE a.locationId IN (%s)
     `
 
-	rows, err := db.Query(query, locationId)
+	query := fmt.Sprintf(baseQuery, placeholders)
+
+	interfaceArgs := make([]interface{}, len(locationIds))
+	for i, v := range locationIds {
+		interfaceArgs[i] = v
+	}
+
+	rows, err := db.Query(query, interfaceArgs...)
+
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	var assets []AssetDetail
+	assetsMap := make(map[int][]AssetDetail)
 	for rows.Next() {
 		var asset AssetDetail
+		var locationId int
 		err = rows.Scan(&asset.id, &asset.productId, &asset.categoryId)
 		if err != nil {
 			return nil, err
 		}
-		assets = append(assets, asset)
+		assetsMap[locationId] = append(assetsMap[locationId], asset)
 	}
 
 	// Check for errors from iterating over rows.
@@ -112,7 +141,43 @@ func fetchAssetsForLocation(locationId int, db *sql.DB) ([]AssetDetail, error) {
 		return nil, err
 	}
 
-	return assets, nil
+	// Print the assets map
+
+	return assetsMap, nil
+}
+func interfaceSlice(slice []int) []interface{} {
+	s := make([]interface{}, len(slice))
+	for i, v := range slice {
+		s[i] = v
+	}
+	return s
+}
+
+func fetchChildLocations(locationIds []int, db *sql.DB) ([]int, error) {
+	fmt.Println("Running child locations func")
+	placeholders := strings.TrimSuffix(strings.Repeat("?,", len(locationIds)), ",")
+	query := `SELECT id FROM Locations WHERE parentId IN (` + placeholders + `)`
+
+	rows, err := db.Query(query, interfaceSlice(locationIds)...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var childIds []int
+	for rows.Next() {
+		var id int
+		if err = rows.Scan(&id); err != nil {
+			return nil, err
+		}
+		childIds = append(childIds, id)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return childIds, nil
 }
 
 type CheckGroupTemplate struct {
@@ -187,8 +252,32 @@ func fetchCheckItemsTemplates(checkGroupTemplatesMap map[int][]CheckGroupTemplat
 
 }
 
+const batchSize = 10
+
 func processAndInsertAssets(assets []AssetDetail, checkGroupTemplatesMap map[int][]CheckGroupTemplate, checkItemTemplatesMap map[int][]CheckItemTemplate, userId string, db *sql.DB) error {
+
+	// Split assets into batches
+	for i := 0; i < len(assets); i += batchSize {
+		end := i + batchSize
+		if end > len(assets) {
+			end = len(assets)
+		}
+
+		batch := assets[i:end]
+
+		err := processBatch(batch, checkGroupTemplatesMap, checkItemTemplatesMap, userId, db)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func processBatch(assets []AssetDetail, checkGroupTemplatesMap map[int][]CheckGroupTemplate, checkItemTemplatesMap map[int][]CheckItemTemplate, userId string, db *sql.DB) error {
 	tx, err := db.Begin()
+
+	fmt.Println("Running the processing of assets")
 
 	if err != nil {
 		return err
@@ -202,16 +291,16 @@ func processAndInsertAssets(assets []AssetDetail, checkGroupTemplatesMap map[int
 		err = tx.Commit()
 	}()
 
-	// Create all of the asset checks with the user Id and the asset Id
-
 	for _, asset := range assets {
+		fmt.Println("Processing asset: ", asset.id)
+
 		assetCheckQuery := `INSERT INTO AssetCheck (assetId, owner, updatedAt, status) VALUES (?, ?, ?, ?)`
 		result, err := tx.Exec(assetCheckQuery, asset.id, userId, time.Now(), "pass")
 		if err != nil {
 			return err
 		}
-		assetCheckId, err := result.LastInsertId()
 
+		assetCheckId, err := result.LastInsertId()
 		if err != nil {
 			return err
 		}
@@ -224,7 +313,6 @@ func processAndInsertAssets(assets []AssetDetail, checkGroupTemplatesMap map[int
 			}
 
 			checkGroupId, err := result.LastInsertId()
-
 			if err != nil {
 				return err
 			}
@@ -236,10 +324,8 @@ func processAndInsertAssets(assets []AssetDetail, checkGroupTemplatesMap map[int
 					return err
 				}
 			}
-
 		}
-
 	}
-	return nil
 
+	return nil
 }
